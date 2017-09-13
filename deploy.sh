ab2eba974c3634472d55fbdb73955bca5e8999ab
Ismail Moghul <ismail.moghul@gmail.com>
2017-09-13 17:15:55 +0100

    Update deploy method

--- deploy.sh
+++ deploy.sh
@@ -2,41 +2,27 @@
 set -e # exit with nonzero exit code if anything fails
 
 # clear and re-create the out directory
-rm -rf www || exit 0;
+rm -rf www;
+rm -f bionode_members.json
+rm -f bionode_hack.json
+rm -f _data.json
+
 mkdir www;
 
-rm _data.json
+# Install dependencies for member.js
+npm install;
 
-node members.js bionode | jq -s '.| {"community": (.[0] - .[1]), "team": .[1] }' > bionode_members.json
-node members.js bionode-hack | jq -s '. | flatten | unique_by(.login) | {"events": . }' > bionode_hack.json
+node $PWD/members.js bionode | "$PWD/bin/jq-linux64" -s '.| {"community": (.[0] - .[1]), "team": .[1] }' > bionode_members.json;
+node $PWD/members.js bionode-hack | "$PWD/bin/jq-linux64" -s '. | flatten | unique_by(.login) | {"events": . }' > bionode_hack.json;
+"$PWD/bin/jq-linux64" -s '. | add' bionode_members.json bionode_hack.json > _data.json;
 
-jq -s '. | add' bionode_members.json bionode_hack.json > _data.json
-
-rm bionode_members.json bionode_hack.json
+rm bionode_members.json bionode_hack.json;
 
 # Need to remove node_modules to avoid a conflict that causes `harp compile` to fail
-rm -r node_modules
+rm -r node_modules;
+
+# Install Harp globally
+npm install -g harp;
 
 # run our compile script, discussed above
-harp compile
-
-# go to the out directory and create a *new* Git repo
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  cd www
-  git init
-
-  # inside this git repo we'll pretend to be a new user
-  git config user.name "Travis CI"
-  git config user.email "travis@bmpvieira.com"
-
-  # The first and only commit to this new Git repo contains all the
-  # files present with the commit message "Deploy to GitHub Pages".
-  git add .
-  git commit -m "Deploy to GitHub Pages"
-
-  # Force push from the current repo's dev branch to the remote github.io
-  # repo's master branch. (All previous history on the master branch
-  # will be lost, since we are overwriting it.) We redirect any output to
-  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
-fi
+harp compile;
